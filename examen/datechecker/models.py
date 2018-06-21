ebb718976d830719f38e731abdd7397fe3037157
s102122 <s102122@ap.be>
2018-06-21 11:21:13 +0200

    django runs!

--- examen/datechecker/models.py
+++ examen/datechecker/models.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class DateChecker(models.Model):
+date1 = models.DateTimefield(black=true, null=true)
+date2 = models.DateTimefield(black=true, null=true)
+date3 = models.DateTimefield(black=true, null=true)
+result = models.CharField(max_length=2)
--- examen/examen/urls.py
+++ examen/examen/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^datechecker/', include('datechecker.urls')),
+    url('admin', admin.site.urls),
+    url('datechecker', include('datechecker.urls')),
 ]
